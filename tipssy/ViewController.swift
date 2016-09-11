2ab2c3b1330cdfa0a43c47d245828cc25987a354
Edmund Li <edmund.xz.lee@gmail.com>
2016-09-11 16:51:00 -0700

    Initial Commit

---
+++ tipssy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipssy
+//
+//  Created by edmund on 9/11/16.
+//  Copyright © 2016 edmund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
